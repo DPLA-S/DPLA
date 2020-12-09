86b9e82869d5a5ad58e1becd7d1b6ca3d0b17bcf
repl.it user <69394951+programmeruser2@users.noreply.github.com>
2020-12-09 01:42:25 +0000

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import lex from './lexer.js';
 import parse from './parser.js';
-import transpile from './transpiler.js';
+import transpileAst from './transpiler.js';
 
-const $ = selector => document.querySelector(selector);
-HTMLElement.prototype.on = HTMLElement.prototype.addEventListener;
-
-$('#run-btn').on('click', e => {
-	const code = $('#code').value;
+function transpile(code) {
 	const tokens = lex(code);
-	const statements = parse(tokens);
-	const jsCode = transpile(statements);
-	$('#js-code').value = jsCode;
-	eval(jsCode);
-});
+	const statements = parse(code);
+	const jsCode = transpileAst(statements);
+	return jsCode;
+}
+function run(code) {
+	const jsCode = transpile(code);
+	return eval(jsCode);
+}
+export transpile, run;
